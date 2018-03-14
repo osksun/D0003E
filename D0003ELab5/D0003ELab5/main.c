8a43dbb3e3933313208e731b0acb74d1770abed3
osksun <37369260+osksun@users.noreply.github.com>
2018-03-14 13:50:41 +0100

    Add files via upload

---
+++ D0003ELab5/D0003ELab5/main.c
@@ -0,0 +1,18 @@
+/*
+ * D0003ELab5.c
+ *
+ * Created: 2018-03-14 13:47:46
+ * Author : osksun-6
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
